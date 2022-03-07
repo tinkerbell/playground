ddced1964fc869f3f9a892246db52db7fe26aed1
Manuel Mendez <mmendez@equinix.com>
2022-03-07 12:14:37 -0500

    nix: Format shell.nix with nixfmt
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- shell.nix
+++ shell.nix
@@ -1,18 +1,24 @@
 let _pkgs = import <nixpkgs> { };
-in
-{ pkgs ?
-  import
-    (_pkgs.fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs";
-      #branch@date: master@2021-03-18
-      rev = "800a3dd90970a277e3f6853633bd7faf04d6691e";
-      sha256 = "07gzvf3m3vh9kkig2x9iybv8yr04kvmrx665z0fb0n5h55ha4v8y";
-    }) { }
-}:
+in { pkgs ? import (_pkgs.fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs";
+  #branch@date: master@2021-03-18
+  rev = "800a3dd90970a277e3f6853633bd7faf04d6691e";
+  sha256 = "07gzvf3m3vh9kkig2x9iybv8yr04kvmrx665z0fb0n5h55ha4v8y";
+}) { } }:
 
 with pkgs;
 
 mkShell {
-  buildInputs = [ go nodePackages.prettier jq shellcheck shfmt terraform_0_14 gpgme packer vagrant ];
+  buildInputs = [
+    go
+    nodePackages.prettier
+    jq
+    shellcheck
+    shfmt
+    terraform_0_14
+    gpgme
+    packer
+    vagrant
+  ];
 }
