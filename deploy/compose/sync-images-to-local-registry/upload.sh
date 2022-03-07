0379cea9cde842266ac1d2dec399fe6945c72b2e
Manuel Mendez <mmendez@equinix.com>
2022-03-07 17:45:43 -0500

    compose: Overhaul sync-images-to-local-registry/upload.sh script
    Another commit where more than one thing is happening but I don't really
    feel like splitting into many. Changes are as follows:
    
    Use `set -euo pipefail` instead of just `set -o pipefail`:
    -o pipefail w/o -e is basically useless since it won't stop execution, just
    stops the command early, and -u adds to the safety. I got rid of -x because
    it was making the script pretty noisy.
    
    Nicer log/debug messages:
    It was kind of hard to see what skopeo was doing for each sync call so I added
    some nicer to find messages.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- deploy/compose/sync-images-to-local-registry/upload.sh
+++ deploy/compose/sync-images-to-local-registry/upload.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
+
 # This script handles uploading containers from one container registry to another.
 # It assumes the target registry requires username and password authentication.
 
-set -xo pipefail
+set -euo pipefail
 
-main() {
-	local reg_user="$1"
-	local reg_pw="$2"
-	local reg_url="$3"
-	local images_file="$4"
-	# this confusing IFS= and the || is to capture the last line of the file if there is no newline at the end
-	while IFS= read -r img || [ -n "${img}" ]; do
-		# file is expected to have src and dst images delimited by a space
-		local src_img
-		src_img="$(echo "${img}" | cut -d' ' -f1)"
-		local dst_img
-		dst_img="$(echo "${img}" | cut -d' ' -f2)"
-		skopeo copy --all --dest-tls-verify=false --dest-creds="${reg_user}":"${reg_pw}" docker://"${src_img}" docker://"${reg_url}"/"${dst_img}"
-	done <"${images_file}"
-}
+user=$1
+pass=$2
+url=$3
+images=$4
 
-main "$1" "$2" "$3" "$4"
+mapfile -t lines <"$images"
+printf "syncing:\n" >&2
+printf "%s\n" "${lines[@]}" | sed 's| | → |' >&2
+for l in "${lines[@]}"; do
+	read -r src dest <<<"$l"
+	echo "::::: syncying $src → $url/$dest :::::" >&2
+	skopeo copy --all --dest-tls-verify=false --dest-creds="$user:$pass" "docker://$src" "docker://$url/$dest"
+done
