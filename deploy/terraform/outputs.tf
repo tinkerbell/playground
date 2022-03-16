f4d3a3e058d294bc9725f6a72566a5eb3f4b8e24
Manuel Mendez <mmendez@equinix.com>
2022-03-16 17:15:38 -0400

    tf: Add output for worker_macs
    Makes it easy to get the macs and make use of, for example look at boots logs.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -6,6 +6,10 @@ output "provisioner_ssh" {
   value = format("%s.packethost.net", split("-", metal_device.tink_provisioner.id)[0])
 }
 
+output "worker_macs" {
+  value = local.worker_macs
+}
+
 output "worker_sos" {
   value = formatlist("%s@sos.%s.platformequinix.com", metal_device.tink_worker[*].id, metal_device.tink_worker.deployed_facility)
 }
