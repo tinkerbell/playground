6123d9efd72726e956a81db3e2e45d2da2105eb4
Manuel Mendez <mmendez@equinix.com>
2022-03-16 16:00:46 -0400

    tf/setup: Only explicitly install the docker packages
    Let dpkg package dependencies and apt-get figure out dependencies.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- deploy/terraform/setup.sh
+++ deploy/terraform/setup.sh
@@ -6,7 +6,7 @@ install_docker() {
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	update_apt
-	apt-get install apt-transport-https ca-certificates curl gnupg-agent gnupg2 software-properties-common docker-ce docker-ce-cli containerd.io
+	apt-get install --no-install-recommends containerd.io docker-ce docker-ce-cli
 }
 
 install_docker_compose() {
