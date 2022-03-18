0f9d3c7976d31ec8f729f2181e7ad81d39fcd0c9
Manuel Mendez <mmendez@equinix.com>
2022-03-18 16:01:44 -0400

    vagrant: Move all provisioner code into just one script
    This just seems easier to manage one script instead of many ruby blocks. Bonus
    points for having it look similar to terraform/setup.sh so common things look
    the same(ish).
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

---
+++ deploy/vagrant/setup.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+setup_layer2_network() {
+	host_addr=$1
+	ip addr show dev eth1 | grep -q "$host_addr" && return 0
+	ip addr add "$host_addr/24" dev eth1
+	ip link set dev eth1 up
+}
+
+setup_compose_env_overrides() {
+	local host_addr=$1
+	local worker_addr=$2
+	if lsblk | grep -q vda; then
+		sed -i 's|sda|vda|g' /vagrant/compose/create-tink-records/manifests/template/ubuntu.yaml
+	fi
+	readarray -t lines <<-EOF
+		TINKERBELL_HOST_IP="$host_addr"
+		TINKERBELL_CLIENT_IP="$worker_addr"
+	EOF
+	for line in "${lines[@]}"; do
+		grep -q "$line" /vagrant/compose/.env && continue
+		echo "$line" >>/vagrant/compose/.env
+	done
+}
+
+create_tink_helper_script() {
+	mkdir -p ~vagrant/.local/bin
+	cat >~vagrant/.local/bin/tink <<-'EOF'
+		#!/usr/bin/env bash
+
+		exec docker-compose -f /vagrant/compose/docker-compose.yml exec tink-cli tink "$@"
+	EOF
+	chmod +x ~vagrant/.local/bin/tink
+}
+
+tweak_bash_interactive_settings() {
+	grep -q 'cd /vagrant/compose' ~vagrant/.bashrc || echo 'cd /vagrant/compose' >>~vagrant/.bashrc
+	readarray -t aliases <<-EOF
+		dc=docker-compose
+	EOF
+	for alias in "${aliases[@]}"; do
+		grep -q "$alias" ~vagrant/.bash_aliases || echo "alias $alias" >>~vagrant/.bash_aliases
+	done
+}
+
+main() {
+	local host_addr=$1
+	local worker_addr=$2
+
+	setup_layer2_network "$host_addr"
+
+	setup_compose_env_overrides "$host_addr" "$worker_addr"
+	docker-compose -f /vagrant/compose/docker-compose.yml up -d
+
+	create_tink_helper_script
+	tweak_bash_interactive_settings
+}
+
+if [[ ${BASH_SOURCE[0]} == "$0" ]]; then
+	set -euxo pipefail
+
+	main "$@"
+fi
