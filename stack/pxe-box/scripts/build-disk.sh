9f7b3d0d249efb4407b8f4901b93fd61f42b7935
Jacob Weinstock <jacobweinstock@users.noreply.github.com>
2026-05-11 19:16:30 -0600

    Update PXE boot URLs for amd64 and arm64 architectures
    go back to pure iPXE binaries.

--- stack/pxe-box/scripts/build-disk.sh
+++ stack/pxe-box/scripts/build-disk.sh
@@ -13,11 +13,11 @@ out=${2:?output path required}
 
 case "$arch" in
 amd64)
-	url="https://boot.netboot.xyz/ipxe/netboot.xyz-snp.efi"
+	url="https://boot.ipxe.org/x86_64-efi/ipxe.efi"
 	loader="BOOTX64.EFI"
 	;;
 arm64)
-	url="https://boot.netboot.xyz/ipxe/netboot.xyz-arm64-snp.efi"
+	url="https://boot.ipxe.org/arm64-efi/ipxe.efi"
 	loader="BOOTAA64.EFI"
 	;;
 *)
